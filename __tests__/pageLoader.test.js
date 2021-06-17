d07d2fe713c4ec9df011a488d84687aa327c93b5
Anatolii Burtsev <anatolyburtsev@yandex.ru>
2021-06-16 21:47:29 -0700

    add back test on unavaiable url

--- __tests__/pageLoader.test.js
+++ __tests__/pageLoader.test.js
@@ -1,5 +1,5 @@
 import fs from 'fs/promises';
-// import { readFileSync } from 'fs';
+import { readFileSync } from 'fs';
 import path from 'path';
 import os from 'os';
 import nock from 'nock';
@@ -11,10 +11,10 @@ import pageLoader from '../src/index.js';
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
-// const loadFixture = (filename) => {
-//   const pathToFixtures = path.resolve(__dirname, '../__fixtures__/', filename);
-//   return readFileSync(pathToFixtures, 'utf8');
-// };
+const loadFixture = (filename) => {
+  const pathToFixtures = path.resolve(__dirname, '../__fixtures__/', filename);
+  return readFileSync(pathToFixtures, 'utf8');
+};
 
 let outputDir = '';
 
@@ -34,6 +34,32 @@ describe('tests on page loader', () => {
 
   afterEach(() => {
     nock.cleanAll();
+  });
+
+  test('should return error if url is unavailable', async () => {
+    const baseUrl = 'https://ru.hexlet.io';
+    const url = 'https://ru.hexlet.io/courses';
+    const expectedScriptContent = loadFixture('runtime.js');
+    const expectedImageContent = loadFixture('nodejs.png');
+
+    nock(baseUrl)
+        .get('/courses')
+        .twice()
+        .reply(200, loadFixture('website.html'));
+    nock(baseUrl).get('/assets/professions/nodejs.png')
+        .reply(200, expectedImageContent, {
+          'content-type': 'application/octet-stream',
+          'content-length': expectedImageContent.length,
+          'content-disposition': 'attachment; filename=nodejs.png',
+        });
+    nock(baseUrl).get('/packs/js/runtime.js')
+        .reply(200, expectedScriptContent);
+    nock(baseUrl).get('/assets/application.css')
+        .reply(500, {});
+
+    await expect(pageLoader(url, outputDir)).rejects
+        .toThrowError();
+    // .toThrowError(/Failed to download several resources: ru-hexlet-io-assets-application.css/);
   });
 
   test('should return error if server returns 4XX', async () => {

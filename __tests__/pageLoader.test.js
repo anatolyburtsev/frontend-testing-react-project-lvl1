92539deb0dd50bbda71927e73d2507c5bef53386
Anatolii Burtsev <anatolyburtsev@yandex.ru>
2021-06-16 19:04:04 -0700

    rollback part of negative pageLoader tests

---
+++ __tests__/pageLoader.test.js
@@ -0,0 +1,52 @@
+import fs from 'fs/promises';
+import { readFileSync } from 'fs';
+import path from 'path';
+import os from 'os';
+import nock from 'nock';
+// import { fileURLToPath } from 'url';
+// eslint-disable-next-line
+import * as axiosdebuglog from 'axios-debug-log';
+import pageLoader from '../src/index.js';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+const loadFixture = (filename) => {
+  const pathToFixtures = path.resolve(__dirname, '../__fixtures__/', filename);
+  return readFileSync(pathToFixtures, 'utf8');
+};
+
+let outputDir = '';
+
+const checkFile = async (filePath, fileContent) => {
+  const content = readFileSync(filePath, 'utf-8');
+  expect(content).toEqual(fileContent);
+};
+
+describe('tests on page loader', () => {
+  beforeEach(async () => {
+    outputDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+  });
+
+  beforeAll(() => {
+    nock.disableNetConnect();
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  test('should return error if server returns 4XX', async () => {
+    const url = 'http://ya.ru';
+    nock(url)
+      .get('/')
+      .reply(404, {});
+    await expect(pageLoader(url, outputDir)).rejects.toThrowError(/Request failed/);
+  });
+
+  test("should return error if doesn't have write permissions to the output dir",
+    async () => {
+      const notWritablePath = '/proc';
+      await expect(pageLoader('http://ya.ru/', notWritablePath)).rejects.toThrowError(/No permissions to write/);
+    });
+});

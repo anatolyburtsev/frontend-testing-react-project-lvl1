816c2f2b77f21450724a3c14d65bb1fcd299f0bf
Anatolii Burtsev <anatolyburtsev@yandex.ru>
2021-06-27 21:54:12 -0700

    small refactor of tests

--- __tests__/pageLoader.test.js
+++ __tests__/pageLoader.test.js
@@ -2,7 +2,6 @@ import fs from 'fs/promises';
 import path from 'path';
 import os from 'os';
 import nock from 'nock';
-import _ from 'lodash';
 import pageLoader from '../src/index.js';
 
 const loadFixture = (filename) => {
@@ -14,29 +13,8 @@ let outputDir = '';
 const baseUrl = 'https://ru.hexlet.io';
 const urlPath = '/courses';
 const url = `${baseUrl}${urlPath}`;
-const assetsInitial = Object.freeze({
-  image: {
-    fixturePath: 'expected/nodejs.png',
-    expectedPath: 'ru-hexlet-io-assets-professions-nodejs.png',
-    url: '/assets/professions/nodejs.png',
-  },
-  css: {
-    fixturePath: 'expected/application.css',
-    expectedPath: 'ru-hexlet-io-assets-application.css',
-    url: '/assets/application.css',
-  },
-  script: {
-    fixturePath: 'expected/runtime.js',
-    expectedPath: 'ru-hexlet-io-packs-js-runtime.js',
-    url: '/packs/js/runtime.js',
-  },
-  html: {
-    fixturePath: 'website.html',
-    expectedPath: 'ru-hexlet-io-courses.html',
-    url: urlPath,
-    times: 2,
-  },
-});
+let assets;
+
 const filesDir = 'ru-hexlet-io-courses_files';
 
 nock.disableNetConnect();
@@ -47,25 +25,48 @@ afterEach(() => {
 
 beforeEach(async () => {
   outputDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+  assets = Object.freeze({
+    image: {
+      fixturePath: 'expected/nodejs.png',
+      expectedPath: 'ru-hexlet-io-assets-professions-nodejs.png',
+      url: '/assets/professions/nodejs.png',
+    },
+    css: {
+      fixturePath: 'expected/application.css',
+      expectedPath: 'ru-hexlet-io-assets-application.css',
+      url: '/assets/application.css',
+    },
+    script: {
+      fixturePath: 'expected/runtime.js',
+      expectedPath: 'ru-hexlet-io-packs-js-runtime.js',
+      url: '/packs/js/runtime.js',
+    },
+    html: {
+      fixturePath: 'website.html',
+      expectedPath: 'ru-hexlet-io-courses.html',
+      url: urlPath,
+    },
+    link: {
+      fixturePath: 'website.html',
+      expectedPath: 'ru-hexlet-io-courses.html',
+      url: urlPath,
+    },
+  });
 });
 
 describe('page loader, positive cases', () => {
-  let assets = {};
   beforeEach(async () => {
-    assets = _.cloneDeep(assetsInitial);
     await Promise.all(Object.entries(assets).map(async ([assetName, asset]) => {
       const fixtureContent = await loadFixture(asset.fixturePath);
-      const times = asset.times ? asset.times : 1;
       const scope = nock(baseUrl)
         .get(asset.url)
-        .times(times)
         .reply(200, fixtureContent);
       assets[assetName].scope = scope;
       assets[assetName].fixtureContent = fixtureContent;
     }));
   });
 
-  test.each(Object.keys(assetsInitial))('verify that %s saved correctly', async (assetType) => {
+  test.each(['image', 'css', 'script', 'html'])('verify that %s saved correctly', async (assetType) => {
     const asset = assets[assetType];
     await pageLoader(url, outputDir);
     const content = await fs.readFile(path.join(outputDir, filesDir, asset.expectedPath), 'utf-8');
@@ -96,7 +97,7 @@ describe('page loader, negative cases', () => {
     nock(baseUrl)
       .get(urlPath)
       .reply(200, await loadFixture('website.html'));
-    Object.values(assetsInitial).forEach((asset) => {
+    Object.values(assets).forEach((asset) => {
       nock(baseUrl)
         .get(asset.url)
         .reply(404, {});
